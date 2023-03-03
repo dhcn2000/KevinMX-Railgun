fb6313e98d6cd05eae149149d3476901891d0cde
dhcn2000 <112333167+dhcn2000@users.noreply.github.com>
2023-03-03 19:00:27 +0800

    Update diy-before-feeds.sh

--- diy/ramips_mt7621/phicomm_k2p/diy-before-feeds.sh
+++ diy/ramips_mt7621/phicomm_k2p/diy-before-feeds.sh
@@ -2,3 +2,5 @@
 
 # Add mtkiappd support for 802.11 k/v/r
 svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/mt/mtkiappd package/kernel/mt-drivers/mtkiappd
+
+git clone -b 18.06 https://github.com/riverscn/luci-app-omcproxy.git package/luci-app-omcproxy
